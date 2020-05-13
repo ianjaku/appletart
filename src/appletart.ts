fc7a95a123aa5303a7d8183199763e074f131d9a
Ian <ian@invacto.com>
2020-05-13 20:04:14 +0200

    added export for installPlugin to appletart

--- src/appletart.ts
+++ src/appletart.ts
@@ -1,7 +1,10 @@
 import controller from './controller'
+import plugins from './plugins'
 
 export const createController = controller.createController
+export const installPlugin = plugins.installPlugin
 
 export default {
   createController: controller.createController,
+  installPlugin: plugins.installPlugin
 }
